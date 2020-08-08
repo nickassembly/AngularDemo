b969713b698353b8dfaad4e64ef36d99dc59944b
Nick Guerra <nickg@maliciousgames.com>
2020-08-08 10:50:27 -0500

    added basic components

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  // template: `<h1>I am a card</h1>`,
+  templateUrl: 'property-card.component.html',
+  //styles: []
+  styleUrls: ['property-card.component.css']
+})
+export class PropertyCardComponent { }
