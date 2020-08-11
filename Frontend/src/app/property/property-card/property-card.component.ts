0fd8628edce774384a2cad4b4d7cece3df5ff3c4
Nick Guerra <nickg@maliciousgames.com>
2020-08-10 19:51:13 -0500

    added property list

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,17 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
-  // template: `<h1>I am a card</h1>`,
   templateUrl: 'property-card.component.html',
-  //styles: []
   styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent {
-  Property: any = {
-    "Id": 1,
-    "Name": "PB Dungeon",
-    "Type": "House",
-    "Price": 120000
-  }
+  @Input() property: any
+
+
 }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name": "PB Dungeon",
+      "Type": "House",
+      "Price": 120000
+    },
+    {
+      "Id": 2,
+      "Name": "Keller Cave",
+      "Type": "House",
+      "Price": 67000
+    },
+    {
+      "Id": 3,
+      "Name": "Burger Mansion",
+      "Type": "House",
+      "Price": 1000000
+    },
+    {
+      "Id": 4,
+      "Name": "Georgia House",
+      "Type": "House",
+      "Price": 86000
+    },
+    {
+      "Id": 5,
+      "Name": "Idaho House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 6,
+      "Name": "Walmart Store",
+      "Type": "House",
+      "Price": 900000
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
