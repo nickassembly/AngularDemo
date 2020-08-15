5625acbec215c69b44b4d56be5362b7034fdbe89
Nick Guerra <nickg@maliciousgames.com>
2020-08-14 19:05:33 -0500

    add property and nav bar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HousingService } from './services/housing.service';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AddPropertyComponent } from './add-property/add-property.component';
 
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent }
+]
 
 @NgModule({
-  declarations: [AppComponent, PropertyCardComponent, PropertyListComponent, NavBarComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent,
+    AddPropertyComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [HousingService],
   bootstrap: [AppComponent],
 })
