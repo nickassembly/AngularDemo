b683497673e3065dd98765b49fcc67c0f505e161
Nick Guerra <nickg@maliciousgames.com>
2020-08-15 06:50:11 -0500

    update routing

--- Frontend/src/app/add-property/add-property.component.ts
+++ Frontend/src/app/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-property',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPropertyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+  }
+
+  onBack() {
+    this.router.navigate(['/']);
   }
 
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,10 +10,13 @@ import { PropertyCardComponent } from './property/property-card/property-card.co
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AddPropertyComponent } from './add-property/add-property.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
 
 const appRoutes: Routes = [
   { path: '', component: PropertyListComponent },
-  { path: 'add-property', component: AddPropertyComponent }
+  { path: 'rent-property', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  { path: 'property-detail/:id', component: PropertyDetailComponent }
 ]
 
 @NgModule({
@@ -22,7 +25,8 @@ const appRoutes: Routes = [
     PropertyCardComponent,
     PropertyListComponent,
     NavBarComponent,
-    AddPropertyComponent],
+    AddPropertyComponent,
+    PropertyDetailComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
